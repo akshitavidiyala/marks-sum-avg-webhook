49bfc59fe300dad48eae69666e940998a0c08489
akshitavidiyala <akshita.vidiyala@gmail.com>
2025-12-01 19:37:29 +0530

    Initial commit: marks sum and average

---
+++ marks.py
@@ -0,0 +1,11 @@
+# marks.py
+
+numbers = [73, 90, 87, 67]  # you can change the numbers
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Marks:", numbers)
+print("Sum:", total)
+print("Average:", average)
+
